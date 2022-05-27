a85af2ee1742e799294d2c050f822301f1718345
Lindsey <linlidiaramirez@gmail.com>
2022-05-26 21:29:22 -0400

    can see battery gallery now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainGallery from './components/MainGallery';
 
 function App() {
   return (
-    <div className="App">
+  <section className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+        Rad Power Battery Repair
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+               
+        <div className='picContainer'>
+         <MainGallery/>
+         </div>   
       </header>
-    </div>
+    </section>
   );
 }
 
---
+++ src/components/MainGallery.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+const MainGallery = props => {
+
+    const slider = (
+        <AutoplaySlider
+          play={true}
+          cancelOnInteraction={false} // should stop playing on user interaction
+          interval={6000}
+        >
+          
+          <div data-src="./bat1.jpg" />
+          <div data-src="./bat2.jpg" />
+          <div data-src="./bat3.jpg" />
+          <div data-src="./bat4.jpg" />
+          <div data-src="./bat5.jpg" />
+        </AutoplaySlider>
+      );
+
+    return (
+        <div className="flex justify-center">
+            {slider}
+        </div>
+    );
+}
+
+export default MainGallery;
