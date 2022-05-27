398e534030bdff908c9a94539f44a24db35851fb
Lindsey <linlidiaramirez@gmail.com>
2022-05-26 22:50:43 -0400

    added 2 sections to style n add to

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainGallery from './components/MainGallery';
+import About from './components/About';
+import GetQuote from './components/GetQuote';
 
 function App() {
   return (
   <section className="App">
       <header className="App-header">
-      <p>
+      <p className="text-3xl font-bold underline">
         Rad Power Battery Repair
         </p>
-               
+               <div className='containER'>
         <div className='picContainer'>
          <MainGallery/>
-         </div>   
+         </div> 
+         <About/> 
+         <GetQuote/> 
+         </div>
       </header>
     </section>
   );
---
+++ src/components/GetQuote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GetQuote = () => {
+    return(
+        <div className='gqContainer'>
+            <p>Get q form</p>
+        </div>
+    );
+
+}
+
+export default GetQuote;
