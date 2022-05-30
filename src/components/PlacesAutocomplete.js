9d12f285db9d1913555e4f0fa38bf07723dd4ddc
Lindsey <linlidiaramirez@gmail.com>
2022-05-30 19:11:04 -0400

    address auto-complete wip

--- src/components/PlacesAutocomplete.js
+++ src/components/PlacesAutocomplete.js
@@ -6,68 +6,65 @@ import usePlacesAutocomplete, {
   import useOnclickOutside from "react-cool-onclickoutside";
   import Form from 'react-bootstrap/Form';
 import { FormControl } from 'react-bootstrap';
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption
+} from "@reach/combobox";
+
   
   const PlacesAutocomplete = () => {
     const {
       ready,
       value,
       suggestions: { status, data },
-      setValue,
-      clearSuggestions,
-    } = usePlacesAutocomplete({
-      requestOptions: {
-        /* Define search scope here */
-      },
-      debounce: 300,
-    });
-    const ref = useOnclickOutside(() => {
-      // When user clicks outside of the component, we can dismiss
-      // the searched suggestions by calling this method
-      clearSuggestions();
-    });
+      setValue
+    } = usePlacesAutocomplete();
   
-    const handleInput = (e) => {
-      // Update the keyword of the input element
+    const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
       setValue(e.target.value);
     };
   
-    const handleSelect =
-      ({ description }) =>
-      () => {
-        // When user selects a place, we can replace the keyword without request data from API
-        // by setting the second parameter to "false"
-        setValue(description, false);
-        clearSuggestions();
+    const handleSelect = (val: string): void => {
+      setValue(val, false);
+    };
   
-        // Get latitude and longitude via utility functions
-        getGeocode({ address: description }).then((results) => {
-          const { lat, lng } = getLatLng(results[0]);
-          console.log("📍 Coordinates: ", { lat, lng });
-        });
-      };
+    const renderSuggestions = (): JSX.Element => {
+      const suggestions = data.map(({ place_id, description }: any) => (
+        <ComboboxOption key={place_id} value={description} />
+      ));
   
-    const renderSuggestions = () =>
-      data.map((suggestion) => {
-        const {
-          place_id,
-          structured_formatting: { main_text, secondary_text },
-        } = suggestion;
-  
-        return (
-          <li key={place_id} onClick={handleSelect(suggestion)}>
-            <strong>{main_text}</strong> <small>{secondary_text}</small>
+      return (
+        <div>
+          {suggestions}
+          <li className="logo">
+            <img
+              src="https://developers.google.com/maps/documentation/images/powered_by_google_on_white.png"
+              alt="Powered by Google"
+            />
           </li>
-        );
-      });
+        </div>
+      );
+    };
   
     return (
-      <div ref={ref}>
-      <Form.Group onChange={handleInput} value={value}>
-                <Form.Control placeholder="Your Shipping Address" />
-            </Form.Group>
-      
-        {/* We can use the "status" to decide whether we should display the dropdown or not */}
-        {status === "OK" && <ul>{renderSuggestions()}</ul>}
+      <div className="App">
+        
+        <Combobox onSelect={handleSelect} aria-labelledby="demo">
+          <ComboboxInput
+            style={{ width: 300, maxWidth: "90%" }}
+            value={value}
+            onChange={handleInput}
+            disabled={!ready}
+            placeholder="Enter Shipping Address"
+          />
+          <ComboboxPopover>
+            <ComboboxList>{status === "OK" && renderSuggestions()}</ComboboxList>
+          </ComboboxPopover>
+        </Combobox>
       </div>
     );
   };
